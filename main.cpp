964a9f82217daa6988afc8b366e565460dfae858
leandro correia <leandronunescorreia@outlook.com>
2025-10-16 21:31:47 +0000

    Enhance main.cpp with WebGPU adapter request functionality

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 // Includes
 #include <webgpu/webgpu.h>
+
+#ifdef __EMSCRIPTEN__
+#  include <emscripten.h>
+#endif // __EMSCRIPTEN__
+
 #include <iostream>
 
 int main (int, char**) {
@@ -23,6 +28,39 @@ int main (int, char**) {
     // Display the object (WGPUInstance is a simple pointer, it may be
     // copied around without worrying about its size).
     std::cout << "WGPU instance: " << instance << std::endl;
+
+	WGPURequestAdapterOptions adapterOpts = {};
+	adapterOpts.nextInChain = nullptr;
+	WGPUAdapter adapter = {};
+
+	auto onAdapterRequestEnded = [](WGPURequestAdapterStatus status, WGPUAdapter adapter, WGPUStringView message, WGPU_NULLABLE void* userdata1, WGPU_NULLABLE void* userdata2) {
+		WGPUAdapter& userData = *reinterpret_cast<WGPUAdapter*>(userdata1);
+        if(userdata2 || message.length > 0) {
+            std::cout << "Userdata2 is not null!" << std::endl;
+        }
+		if (status == WGPURequestAdapterStatus_Success) {
+			userData = adapter;
+		} else {
+            std::cout << "Could not get WebGPU adapter: " << std::endl;
+		}
+	};
+    WGPURequestAdapterCallbackInfo callbackInfo = {};
+    callbackInfo.nextInChain = nullptr;
+    callbackInfo.mode = WGPUCallbackMode_WaitAnyOnly;
+    callbackInfo.callback = onAdapterRequestEnded;
+    callbackInfo.userdata1 = (void*)&adapter;
+    // We request the adapter
+	wgpuInstanceRequestAdapter(
+		instance /* equivalent of navigator.gpu */,
+		&adapterOpts,
+        callbackInfo
+	);
+
+    if (!adapter) {
+        std::cerr << "Could not initialize the Adapter!" << std::endl;
+        return 1;
+    }
+
 
     // We clean up the WebGPU instance
     wgpuInstanceRelease(instance);
