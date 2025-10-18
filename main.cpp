e6f2a54fc4d488315e597b4a6c4f007f048bb814
leandro correia <leandronunescorreia@outlook.com>
2025-10-18 09:36:19 +0000

    Add device inspection and feature reporting in main.cpp

--- main.cpp
+++ main.cpp
@@ -35,6 +35,59 @@ const char* WGPUFeatureNameToString(WGPUFeatureName name) {
     }
 }
 
+void inspectDevice(WGPUDevice device) {
+	WGPUSupportedFeatures* supported = new WGPUSupportedFeatures();
+	wgpuDeviceGetFeatures(device, supported);
+
+	std::cout << "Device features:" << std::endl;
+	std::cout << std::hex;
+	for (size_t i = 0; i < supported->featureCount; ++i) {
+		auto f = supported->features[i];
+        std::cout << std::hex;
+		std::cout << " - 0x" << f << std::endl;
+	}
+	std::cout << std::dec;
+
+	WGPULimits limits = {};
+	limits.nextInChain = nullptr;
+
+#ifdef WEBGPU_BACKEND_DAWN
+	bool success = wgpuDeviceGetLimits(device, &limits) == WGPUStatus_Success;
+#else
+	WGPUStatus status = wgpuDeviceGetLimits(device, &limits);
+#endif
+	
+	if (status == WGPUStatus_Success) {
+		std::cout << "Device limits:" << std::endl;
+		std::cout << " - maxTextureDimension1D: " << limits.maxTextureDimension1D << std::endl;
+		std::cout << " - maxTextureDimension2D: " << limits.maxTextureDimension2D << std::endl;
+		std::cout << " - maxTextureDimension3D: " << limits.maxTextureDimension3D << std::endl;
+		std::cout << " - maxTextureArrayLayers: " << limits.maxTextureArrayLayers << std::endl;
+		std::cout << " - maxBindGroups: " << limits.maxBindGroups << std::endl;
+		std::cout << " - maxDynamicUniformBuffersPerPipelineLayout: " << limits.maxDynamicUniformBuffersPerPipelineLayout << std::endl;
+		std::cout << " - maxDynamicStorageBuffersPerPipelineLayout: " << limits.maxDynamicStorageBuffersPerPipelineLayout << std::endl;
+		std::cout << " - maxSampledTexturesPerShaderStage: " << limits.maxSampledTexturesPerShaderStage << std::endl;
+		std::cout << " - maxSamplersPerShaderStage: " << limits.maxSamplersPerShaderStage << std::endl;
+		std::cout << " - maxStorageBuffersPerShaderStage: " << limits.maxStorageBuffersPerShaderStage << std::endl;
+		std::cout << " - maxStorageTexturesPerShaderStage: " << limits.maxStorageTexturesPerShaderStage << std::endl;
+		std::cout << " - maxUniformBuffersPerShaderStage: " << limits.maxUniformBuffersPerShaderStage << std::endl;
+		std::cout << " - maxUniformBufferBindingSize: " << limits.maxUniformBufferBindingSize << std::endl;
+		std::cout << " - maxStorageBufferBindingSize: " << limits.maxStorageBufferBindingSize << std::endl;
+		std::cout << " - minUniformBufferOffsetAlignment: " << limits.minUniformBufferOffsetAlignment << std::endl;
+		std::cout << " - minStorageBufferOffsetAlignment: " << limits.minStorageBufferOffsetAlignment << std::endl;
+		std::cout << " - maxVertexBuffers: " << limits.maxVertexBuffers << std::endl;
+		std::cout << " - maxVertexAttributes: " << limits.maxVertexAttributes << std::endl;
+		std::cout << " - maxVertexBufferArrayStride: " << limits.maxVertexBufferArrayStride << std::endl;
+		std::cout << " - maxInterStageShaderComponents: " << limits.maxInterStageShaderVariables << std::endl;
+		std::cout << " - maxComputeWorkgroupStorageSize: " << limits.maxComputeWorkgroupStorageSize << std::endl;
+		std::cout << " - maxComputeInvocationsPerWorkgroup: " << limits.maxComputeInvocationsPerWorkgroup << std::endl;
+		std::cout << " - maxComputeWorkgroupSizeX: " << limits.maxComputeWorkgroupSizeX << std::endl;
+		std::cout << " - maxComputeWorkgroupSizeY: " << limits.maxComputeWorkgroupSizeY << std::endl;
+		std::cout << " - maxComputeWorkgroupSizeZ: " << limits.maxComputeWorkgroupSizeZ << std::endl;
+		std::cout << " - maxComputeWorkgroupsPerDimension: " << limits.maxComputeWorkgroupsPerDimension << std::endl;
+	}
+}
+
 int main (int, char**) {
     // We create a descriptor
     WGPUInstanceDescriptor desc = {};
@@ -124,10 +177,70 @@ int main (int, char**) {
     std::cout << " - adapterType: 0x" << std::hex << adapterInfo.adapterType << std::dec << std::endl;
     std::cout << " - backendType: 0x" << std::hex << adapterInfo.backendType << std::dec << std::endl;
 
-    // We clean up the WebGPU instance
+
+
+  
+	std::cout << "Requesting device..." << std::endl;
+    WGPUDeviceDescriptor deviceDesc = {};
+    deviceDesc.nextInChain = nullptr;
+    deviceDesc.label = WGPUStringView{"My Device", 9}; // anything works here, that's your call
+    deviceDesc.requiredFeatureCount = 0; // we do not require any specific feature
+	deviceDesc.requiredLimits = nullptr; // we do not require any specific limit
+    deviceDesc.defaultQueue.nextInChain = nullptr;
+    deviceDesc.defaultQueue.label = WGPUStringView{"The default queue", 17};
+
+
+    auto deviceLostCallback = [](WGPUDevice const * device, WGPUDeviceLostReason reason, WGPUStringView message, WGPU_NULLABLE void* userdata1, WGPU_NULLABLE void* userdata2) {
+        std::cout << "Device " << std::hex << device << std::dec << " lost callback invoked, reason: " << std::hex << reason << std::dec;
+        std::cout << ", message: " << std::string(message.data, message.length);
+        if(userdata1 || userdata2) {
+            std::cout << ", userdata not null!";
+        }
+        std::cout << std::endl;
+    };
+
+    WGPUDeviceLostCallbackInfo deviceLostCallbackInfo = {};
+    deviceLostCallbackInfo.nextInChain = nullptr;
+    deviceLostCallbackInfo.mode = WGPUCallbackMode_AllowSpontaneous;
+    deviceLostCallbackInfo.callback = deviceLostCallback;
+    deviceLostCallbackInfo.userdata1 = nullptr;
+    deviceLostCallbackInfo.userdata2 = nullptr;
+
+    deviceDesc.deviceLostCallbackInfo = deviceLostCallbackInfo;
+
+
+	// A function that is invoked whenever the device stops being available.
+	auto onDeviceRequestEnded = [](WGPURequestDeviceStatus status, WGPUDevice device, WGPUStringView message, WGPU_NULLABLE void* userdata1, WGPU_NULLABLE void* userdata2) {
+		WGPUDevice& userData = *reinterpret_cast<WGPUDevice*>(userdata1);
+		if (status == WGPURequestDeviceStatus_Success) {
+			userData = device;
+		} else {
+			std::cout << "Could not get WebGPU device: " << std::string(message.data, message.length) << std::endl;
+		}
+        if(userdata2 || message.length > 0) {
+            std::cout << "Userdata2 is not null!" << std::endl;
+        }        
+	};
+
+    WGPUDevice device = {};
+    WGPURequestDeviceCallbackInfo callbackInfoDevice = {};
+    callbackInfoDevice.nextInChain = nullptr;
+    callbackInfoDevice.mode = WGPUCallbackMode_WaitAnyOnly;
+    callbackInfoDevice.callback = onDeviceRequestEnded;
+    callbackInfoDevice.userdata1 = (void*)&device;
+    callbackInfoDevice.userdata2 = nullptr;
+	wgpuAdapterRequestDevice(
+		adapter,
+		&deviceDesc,
+		callbackInfoDevice
+	);
+	std::cout << "Got device: " << device << std::endl;
+
+    inspectDevice(device);
+
     wgpuInstanceRelease(instance);
     wgpuAdapterRelease(adapter);
-
+	wgpuDeviceRelease(device);
 
     return 0;
 }
